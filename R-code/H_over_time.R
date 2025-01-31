6857b2051fc88b1f18caad3e02fcacb5f03ee598
Kai-ChristianSchulz <kaichrisschulz@gmail.com>
2025-01-31 14:54:42 +0100

    Update H_over_time.R

--- R-code/H_over_time.R
+++ R-code/H_over_time.R
@@ -1,12 +1,13 @@
-# H over time
+# H_over_time.R
 
-# We estimate H over time, using a the same linear regressiom method as in 
-# "Estimating Smoothness", but now with a half year (approx 126 trading days) 
-# rolling window  instead. 
+################################################################################
+# We investigate how H changes over time. First by using the same linear regression 
+# method as in "smoothness.R", but now with a half year (approx 126 trading days)
+# rolling window instead. Second, by splitting the data set into three parts and
+# then applying the linear regression method to each third.
+################################################################################
 
-# We examine the SPX
-
-# We restrict ourself to Delta = 1,...,10
+# Initializing #################################################################
 
 m = function(q, delta, data) {
   sp = 1 # Starting Position
@@ -21,6 +22,17 @@ m = function(q, delta, data) {
 }
 
 OM = read.csv("D:\\Aachen\\Mathe\\12.Semester\\Masterarbeit\\Daten\\oxfordmanrealizedvolatilityindices.csv")
+
+
+################################################################################
+# First, for the SPX index, we use the same linear regression 
+# method as in "smoothness.R", but now with a half year (approx 126 trading days)
+# rolling window instead. We also restrict ourself to lags Delta = 1,...,10.
+# All in all this calculation method quite unstable and not recommended. In the
+# thesis we refer to an alternative method used by Gatheral and Bennedsen.
+################################################################################
+
+# Initializing 
 SPX = OM[OM$Symbol==".SPX",]
 RV_SPX = sqrt( SPX$rv5[! SPX$rv5 %in% 0] )
 q = c(0.5,1,1.5,2,3)
@@ -31,9 +43,8 @@ H = vector()
 window_start = 1
 window_end = 126
 
-# Calculating H value via linear regression for each rolling window
+# Calculating H value via linear regression for the rolling window
 while(window_end <= length(RV_SPX)){
-  
   data = RV_SPX[window_start:window_end]
   output = matrix(nrow = length(q), ncol = length(delta))
   for (i in 1:length(q)) {
@@ -47,7 +58,6 @@ while(window_end <= length(RV_SPX)){
   
   window_start = window_start + 1
   window_end = window_end +1
-  
 }
 
 
@@ -55,20 +65,20 @@ while(window_end <= length(RV_SPX)){
 plot(1:length(H), H, type="l",xaxt = 'n', ylab="H", xlab= "", main="SPX")
 axis(side=1, at = seq(1,length(H), by = 252) , labels =seq(2001,2020, by=1))
 
+################################################################################
+# Second, we split the time horizon into the three parts 2000-2006, 2007-2013 
+# and 2014-2020 and estimate H for each third based on the linear regression 
+# method used in "smoothness.R". We repeat this for all indices in the OM 
+# dataset.
+# Note that not all indices have the same number of RV5 entries, they can 
+# slightly vary. However, individually determining the correct breaks for each 
+# index would be too much effort. We proxy by just dividing the length of each
+# index by 3.
+################################################################################
 
-# Splitting the data set into three parts and calculating H for each  
-# index in each third
-
-# For example for SPX, we would do
-# Entries 1:1749 -> 2000-2006
-# Entries 1750:3511 -> 2007-2013
-# 3512:length(dates) -> 2014-Beginning 2020
-# breaks = c(0,1749,3511,5052)
-# However, not all Indices have the same number of RV5 entries, thus
-# we need to individually calculate the breaks. That would be too much effort,
-# we proxy by just dividing the length of each index by 3.
-
+# Initializing #################################################################
 indices = unique(OM$Symbol)
+
 # Matrix that stores the H values for each index rowwise and each third columnwise
 H_all = matrix(ncol=3,nrow=length(indices)) 
 rownames(H_all) = indices
@@ -76,9 +86,8 @@ rownames(H_all) = indices
 q = c(0.5,1,1.5,2,3)
 delta = seq(1:50)
 
-
+# Calculating H estimates for each index #######################################
 for(index in indices) {
-  
   
   breaks = c(0,1,2,3) * floor(length(OM[OM$Symbol==index,]$rv5)/3)
   
@@ -100,6 +109,7 @@ for(index in indices) {
   
 }
 
+# Saving as a table
 H_all
 write.table(round(H_all,digits=3),"D:\\Aachen\\Mathe\\12.Semester\\Masterarbeit\\R-Programme\\H over time.dat", 
             sep=" & ")
